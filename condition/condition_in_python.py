bbb00b77c6ccd48c2b7f98abab6f0845cc3ca5c6
Jayaram Dhungana <jayaramdhungana104@gmail.com>
2025-11-13 22:52:10 +0545

    else if and ternary in python

---
+++ condition/condition_in_python.py
@@ -0,0 +1,20 @@
+
+age=int(input("Enter your age : "))
+
+# if(age>18):
+#     print("You are eligible for vote")
+# else:
+#     print("you are not eligible for vote")
+
+# if(age<11):
+#     print("You are a child")
+# elif(age<19):
+#     print("You are a teen")
+# elif(age<60):
+#     print("You are a youth")
+# else:
+#     print("You are old man")
+
+#check ternary operator in python
+
+print("You are a youth") if(age>18) else print("You are a child")
